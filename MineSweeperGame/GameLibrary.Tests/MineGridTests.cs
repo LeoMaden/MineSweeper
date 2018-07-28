677165b39340ca7dd60e308af0a6d96f769b0cf5
Leo Maden <leo.c.maden@gmail.com>
2018-07-28 19:26:59 +0100

    Added unit test for MineGrid constuctor

---
+++ MineSweeperGame/GameLibrary.Tests/MineGridTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameLibrary;
+
+namespace GameLibrary.Tests
+{
+    [TestClass]
+    public class MineGridTests
+    {
+        [TestMethod]
+        public void MineGrid_ConstructorWorks()
+        {
+            MineGrid grid = new MineGrid(20, 14, 30);
+
+            // Parameters of grid are correct.
+            Assert.IsTrue(grid.Width == 20);
+            Assert.IsTrue(grid.Height == 14);
+            Assert.IsTrue(grid.NumberOfMines == 30);
+
+            // Throws exception if too many mines
+            Assert.ThrowsException<InvalidOperationException>(() => new MineGrid(20, 14, 113));
+        }
+    }
+}
