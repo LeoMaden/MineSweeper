8ba550d22bb6fe098c2b98e3a6046f7097d5fe0c
Leo Maden <leo.c.maden@gmail.com>
2018-07-28 18:28:38 +0100

    Created MineGrid class in library and added fields. Also made a start on constuctor and PlaceMines method

---
+++ MineSweeperGame/GameLibrary/MineGrid.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary
+{
+    public class MineGrid
+    {
+        private int[,] GameGrid { get; set; }
+
+        public int Width => GameGrid.GetLength(0);
+
+        public int Height => GameGrid.GetLength(1);
+
+        public int NumberOfMines { get; private set; }
+
+
+        public MineGrid(int width, int height, int numMines)
+        {
+            // Initialise fields in constructor.
+            GameGrid = new int[width, height];
+            NumberOfMines = numMines;
+        }
+
+        private void PlaceMines()
+        {
+            // Loop over how many mines there are.
+            for (int i = 0; i < NumberOfMines; i++)
+            {
+                Random rand = new Random();
+
+                int randX = rand.Next(Width);
+                int randY = rand.Next(Height);
+
+
+            }
+        }
+    }
+}
